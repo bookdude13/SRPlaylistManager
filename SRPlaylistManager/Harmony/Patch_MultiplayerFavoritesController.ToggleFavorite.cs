cfc3500c64e10fbf51b0d5637eabeafb1db633bb
Ryan Fredlund <rfredlund13@gmail.com>
2023-03-21 20:41:21 -0500

    Commenting out multiplayer start for now

--- SRPlaylistManager/Harmony/Patch_MultiplayerFavoritesController.ToggleFavorite.cs
+++ SRPlaylistManager/Harmony/Patch_MultiplayerFavoritesController.ToggleFavorite.cs
@@ -9,7 +9,7 @@ using Util.Controller;
 
 namespace SRPlaylistManager.Harmony
 {
-    [HarmonyPatch(typeof(MultiplayerFavoritesController), nameof(MultiplayerFavoritesController.ToggleFavorite))]
+    /*[HarmonyPatch(typeof(MultiplayerFavoritesController), nameof(MultiplayerFavoritesController.ToggleFavorite))]
     public class Patch_MultiplayerFavoritesController_ToggleFavorite
     {
         public static bool Prefix()
@@ -19,5 +19,5 @@ namespace SRPlaylistManager.Harmony
             // Don't follow the normal "Add/Remove Favorites" logic
             return false;
         }
-    }
+    }*/
 }
