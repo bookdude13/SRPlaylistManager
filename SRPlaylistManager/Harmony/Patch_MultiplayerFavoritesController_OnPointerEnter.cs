4df73e7236b66c9be04504c6c69538ad32237d89
Ryan Fredlund <rfredlund13@gmail.com>
2023-10-07 17:56:11 -0500

    Update tooltip in multiplayer

---
+++ SRPlaylistManager/Harmony/Patch_MultiplayerFavoritesController_OnPointerEnter.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+using Il2CppUtil.Controller;
+using UnityEngine.EventSystems;
+
+namespace SRPlaylistManager.Harmony
+{
+    [HarmonyPatch(typeof(MultiplayerFavoritesController), nameof(MultiplayerFavoritesController.OnPointerEnter), new Type[] { typeof(PointerEventData) })]
+    public class Patch_MultiplayerFavoritesController_OnPointerEnter
+    {
+        public static void Postfix(MultiplayerFavoritesController __instance, PointerEventData eventData)
+        {
+            // Override tooltip text
+            __instance.tooltip = "Select Playlists";
+        }
+    }
+}
