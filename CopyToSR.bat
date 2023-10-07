ca2a645a152b2a56c9ea4b041feacb66f0c3bd23
Ryan Fredlund <rfredlund13@gmail.com>
2023-10-07 17:45:34 -0500

    SRModCore as submodule, update build scripts

--- CopyToSR.bat
+++
@@ -1,8 +0,0 @@
-set MOD_NAME=SRPlaylistManager
-
-set BUILT_DLL=".\%MOD_NAME%\bin\Debug\net6.0\%MOD_NAME%.dll"
-set LIB_DLL_DIR=".\%MOD_NAME%\bin\Debug\net6.0\libs"
-set SYNTHRIDERS_MODS_DIR="C:\Program Files (x86)\Steam\steamapps\common\SynthRiders\Mods"
-
-copy %BUILT_DLL% %SYNTHRIDERS_MODS_DIR%
-copy %LIB_DLL_DIR%\* %SYNTHRIDERS_MODS_DIR%
